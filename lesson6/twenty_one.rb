8421cdc04be8960d22c4822acea807767c64a613
Joseph McGroarty <joeymcgroarty@gmail.com>
2018-09-23 03:30:36 -0400

    21 bonus features 4 and 5 maybe 3

--- lesson6/twenty_one.rb
+++ lesson6/twenty_one.rb
@@ -1,3 +1,6 @@
+BUST_OVER = 21
+DEALER_HITS_TO = 17
+
 deck_hash = {ace_of_hearts: 11, two_of_hearts: 2, three_of_hearts: 3, four_of_hearts: 4, five_of_hearts: 5, six_of_hearts: 6,
             seven_of_hearts: 7, eight_of_hearts: 8, nine_of_hearts: 9, ten_of_hearts: 10,
             jack_of_hearts: 10, queen_of_hearts: 10, king_of_hearts: 10,
@@ -13,6 +16,10 @@ deck_hash = {ace_of_hearts: 11, two_of_hearts: 2, three_of_hearts: 3, four_of_he
 
 player_hand = {}
 dealer_hand = {}
+
+player_wins = 0
+dealer_wins = 0
+ties = 0
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -40,7 +47,7 @@ def hand_value(hand)
   sum = hand.values.sum
 
   hand.values.select { |value| value == 11}.count.times do
-    sum -= 10 if sum > 21
+    sum -= 10 if sum > BUST_OVER
   end
   sum
 end
@@ -51,65 +58,95 @@ def display_table(dlr, plyr)
 end
 
 def busted?(hand)
-  if hand_value(hand) > 21
+  if hand_value(hand) > BUST_OVER
     true
   else
     false
   end
 end
 
-prompt "Let's play 21. Shuffling cards..."
-prompt "Dealing hands..."
-deal_a_card!(deck_hash, player_hand)
-deal_a_card!(deck_hash, dealer_hand)
-deal_a_card!(deck_hash, player_hand)
-deal_a_card!(deck_hash, dealer_hand)
+def expose_hands(plyr, dlr)
+  prompt "You have #{joiner(plyr.keys)}, which totals #{hand_value(plyr)}."
+    prompt "Dealer has #{joiner(dlr.keys)}, which totals #{hand_value(dlr)}."
+end
 
-# player turn loop
+def play_again?
+  prompt "Play another hand? (y or n)"
+  answer = gets.chomp
+  answer.downcase.start_with?('y')
+end
+
 loop do
-  display_table(dealer_hand, player_hand)
-  prompt "Hit or stay?"
-  player_hit_or_stay = gets.chomp.downcase
-  if player_hit_or_stay == 'hit'
-    deal_a_card!(deck_hash, player_hand)
-  elsif player_hit_or_stay == 'stay'
-    break
-  else
-    prompt "That's not a valid response, try again."
-  end
-  if busted?(player_hand)
-    display_table(dealer_hand, player_hand)
-    prompt "Player busted, dealer wins!"
-    break
-  end
-end
+  player_hand = {}
+  dealer_hand = {}
+  prompt "Let's play #{BUST_OVER}. Shuffling cards..."
+  prompt "Dealing hands..."
+  deal_a_card!(deck_hash, player_hand)
+  deal_a_card!(deck_hash, dealer_hand)
+  deal_a_card!(deck_hash, player_hand)
+  deal_a_card!(deck_hash, dealer_hand)
 
-# dealer turn loop
-if busted?(player_hand) == false
+  # player turn loop
   loop do
-    if hand_value(dealer_hand) < 17
-      deal_a_card!(deck_hash, dealer_hand)
-      prompt "Dealer hits."
+    display_table(dealer_hand, player_hand)
+    prompt "Hit or stay?"
+    player_hit_or_stay = gets.chomp.downcase
+    if player_hit_or_stay == 'hit'
+      deal_a_card!(deck_hash, player_hand)
+    elsif player_hit_or_stay == 'stay'
+      break
     else
-      prompt "Dealer stays."
-      break
+      prompt "That's not a valid response, try again."
     end
-    if busted?(dealer_hand)
-      prompt "Dealer busted, player wins!"
+    if busted?(player_hand)
+      expose_hands(player_hand, dealer_hand)
+      prompt "Player busted, dealer wins!"
+      dealer_wins += 1
       break
     end
   end
-end
 
-# if no busts, compare hands and declare winner
-if busted?(player_hand) == false && busted?(dealer_hand) == false
-  prompt "You have #{joiner(player_hand.keys)}, which totals #{hand_value(player_hand)}."
-  prompt "Dealer has #{joiner(dealer_hand.keys)}, which totals #{hand_value(dealer_hand)}."
-  if hand_value(player_hand) > hand_value(dealer_hand)
-    prompt "Player wins!"
-  elsif hand_value(player_hand) < hand_value(dealer_hand)
-    prompt "Dealer wins!"
-  else
-    prompt "It's a tie!"
+  # dealer turn loop
+  if busted?(player_hand) == false
+    loop do
+      if hand_value(dealer_hand) < DEALER_HITS_TO
+        deal_a_card!(deck_hash, dealer_hand)
+        prompt "Dealer hits."
+      else
+        prompt "Dealer stays."
+        break
+      end
+      if busted?(dealer_hand)
+        expose_hands(player_hand, dealer_hand)
+        prompt "Dealer busted, player wins!"
+        player_wins += 1
+        break
+      end
+    end
   end
+
+  # if no busts, compare hands and declare winner
+  if busted?(player_hand) == false && busted?(dealer_hand) == false
+    expose_hands(player_hand, dealer_hand)
+    if hand_value(player_hand) > hand_value(dealer_hand)
+      prompt "Player wins!"
+      player_wins += 1
+    elsif hand_value(player_hand) < hand_value(dealer_hand)
+      prompt "Dealer wins!"
+      dealer_wins += 1
+    else
+      prompt "It's a tie!"
+      ties += 1
+    end
+  end
+  if player_wins == 5
+    prompt "Player has won 5 hands. Player wins game!"
+    break
+  elsif dealer_wins == 5
+    prompt "Dealer has won 5 hands. Dealer wins game!"
+    break
+  end
+  prompt "Dealer has won #{dealer_wins} hands, player has won #{player_wins} hands, and player and dealer have tied #{ties} hands."
+  break unless play_again?
 end
+prompt "Thanks for playing!"
